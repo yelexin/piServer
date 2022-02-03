d471bb8f1d64b729fecf1a703ae9ffcf93fc29fd
yelexin <yelexin@hotmail.com>
2022-02-03 10:44:12 +0800

    update

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,7 +1,19 @@
 import * as yaml from 'yaml';
 import * as fs from 'fs';
 
-const yamlConfig = fs.readFileSync('config.yaml', 'utf-8');
+let yamlConfig = null;
+
+if (fs.existsSync('config.yaml')) {
+  yamlConfig = fs.readFileSync('config.yaml', 'utf-8');
+} else if (fs.existsSync('/usr/local/etc/piServer/config.yaml')) {
+  yamlConfig = fs.readFileSync('/usr/local/etc/piServer/config.yaml', 'utf-8');
+} else if (fs.existsSync('/etc/piServer/config.yaml')) {
+  yamlConfig = fs.readFileSync('/etc/piServer/config.yaml', 'utf-8');
+} else {
+  throw new Error(
+    '找不到配置文件，请将配置文件放到项目目录或 /usr/local/etc/piServer/config.yaml 或 /etc/piServer/config.yaml',
+  );
+}
 
 interface IConfig {
   serverOrigin: string;
@@ -20,7 +32,7 @@ interface IConfig {
   session: {
     secret: string;
     dbPath: string;
-  }
+  };
   // sqlite 数据库文件地址
   db: {
     path: string;
